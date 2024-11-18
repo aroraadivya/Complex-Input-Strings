aae953e8f9ddc6a0148410c41b4828e262307e76
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2024-11-18 09:59:50 +0530

    Add files via upload

---
+++ HotelManzana/HotelManzana/HotelManzanaModels.swift
@@ -0,0 +1,19 @@
+//
+//  HotelManzanaModels.swift
+//  HotelManzana
+//
+//  Created by Divya Arora on 18/11/24.
+//
+
+import Foundation
+
+struct registration{
+    var firstName: String
+    var lastName: String
+    var emailAddress: String
+    
+    var checkInDate: Date
+    var checkOutDate: Date
+    var numberOfAdults: Int
+    var numberOfChildren: Int
+}
